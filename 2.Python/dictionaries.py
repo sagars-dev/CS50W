a580e578679f4717c8016105ddffa3e88c561aed
sagar <suvarna.sagar3@gmail.com>
2021-02-10 23:12:56 +0530

    python examples

---
+++ 2.Python/dictionaries.py
@@ -0,0 +1,10 @@
+# Define a dictionary
+houses = {"Harry": "Gryffindor", "Draco": "Slytherin"}
+
+# print all data
+print(f"full list = {houses}")
+# print key value
+print(f" key is {houses['Draco']}")
+# its mutable
+houses["Hermione"] = "Gryffindor"
+print(houses)
---
+++ 2.Python/square.py
@@ -0,0 +1,8 @@
+
+#from methods import square
+import methods as mthd
+from math import ceil,sqrt
+
+for j in range(10):
+    sqr = mthd.square(j)
+    print(f"square of j is {sqr} and sqrt is {sqrt(sqr)}")
